0117edc5c4e93fadba8984a039205ff414735327
Syahrul-Fajar <syhrlfajar@gmail.com>
2025-07-24 17:19:04 +0700

    Portofolio

--- next.config.js
+++ next.config.js
@@ -1,28 +1,16 @@
-// next.config.js
-const path = require('path')
-
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  output: 'export', // Penting: Ini akan menghasilkan file statis ke folder 'out'
-  images: {
-    unoptimized: true, // Ini akan menonaktifkan Image Optimization bawaan Next.js
-    remotePatterns: [{
-        protocol: 'https',
-        hostname: 'res.cloudinary.com',
-        pathname: '**'
-      },
-      {
-        protocol: 'https',
-        hostname: 'media.dev.to',
-        pathname: '**'
-      },
-      {
-        protocol: 'https',
-        hostname: 'media2.dev.to',
-        pathname: '**'
-      },
-    ],
-  },
-}
 
-module.exports = nextConfig
+const nextConfig = {
+  // Mengaktifkan mode ekspor statis
+  output: 'export',
+
+  // Karena deploy ke root (syahrul-fajar.github.io),
+  // basePath dan assetPrefix TIDAK DIPERLUKAN. Hapus atau komentari baris ini.
+
+  // Nonaktifkan optimisasi gambar bawaan Next.js
+  images: {
+    unoptimized: true,
+  },
+};
+
+module.exports = nextConfig;
